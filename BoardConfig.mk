8636cd64587bf582b02b007f597c43ac4fa2daf7
k2wl <k2wl@github.com>
2014-12-25 16:35:25 +0530

    Boradconfig:Remove seperator error for wlan module
    Change-Id: Ie677f7a26ab19202db4f23427e4711736ec0f67e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,9 +57,9 @@ TARGET_KERNEL_SOURCE := kernel/motorola/titan
 TARGET_KERNEL_CONFIG := titan_defconfig
 
 WLAN_MODULES:
-        mkdir -p $(KERNEL_MODULES_OUT)/pronto
-        mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
-        ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
+	mkdir -p $(KERNEL_MODULES_OUT)/pronto
+	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
 
 TARGET_KERNEL_MODULES += WLAN_MODULES
 
